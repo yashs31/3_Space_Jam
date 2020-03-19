901d01529446845fe48ecbd16f3d5a0269b7c313
yash31 <yashsavoicar@gmail.com>
2020-03-19 15:57:20 +0530

    added level,sounds,particle_systems,refactored code

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,14 +1,25 @@
-﻿using System;
+﻿using System.Threading;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {   
     [SerializeField] float rcsThrust=100f;
     [SerializeField] float mainThrust=20f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip success;
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
+    enum State{Alive,Dying,Transcending};
+    State state=State.Alive;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,28 +30,60 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if(state==State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if(state!=State.Alive)
+        {
+            return;
+        }
         switch(collision.gameObject.tag)
         {
             case "Friendly":
-                    print("launch pad collided");
                     break;
-            case "Fuel":
-                    print("Fuel");
-                    break;  
+            case "Finish":
+                StartSuccessProcess();//method called after 1s
+                break;
             default:
-                print("dead");
                 //kill
-                break;              
+                StartDeathProcess();
+                break;
         }
     }
 
-    private void Rotate()
+    private void StartSuccessProcess()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        successParticles.Play();
+        Invoke("LoadNextLevel", 1f);
+    }
+    private void StartDeathProcess()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstLevel", 1f);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void RespondToRotateInput()
     {   
         rigidBody.freezeRotation=true;          //manual rotation control
         
@@ -58,19 +101,26 @@ public class Rocket : MonoBehaviour
 
         rigidBody.freezeRotation=false;         //resume physics control
     }
-    private void Thrust()
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up*mainThrust);    //relative force means if ship is tilted,force is in y of ship not actual y
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            ApplyThrust();
         }
         else
         {
             audioSource.Stop();
+            mainEngineParticles.Stop();
         }
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);    //relative force means if ship is tilted,force is in y of ship not actual y
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play();
     }
 }
